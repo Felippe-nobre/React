bae788bb5a17a7155751629a4270292273681f3f
Felippe <113641116+Felippe-nobre@users.noreply.github.com>
2026-02-25 11:41:40 -0300

    Finalização do modulo 4 que fizemos a pagina de video

---
+++ cinetag/src/pages/NaoEncontrada/index.js
@@ -0,0 +1,12 @@
+import styles from './NaoEncontrada.module.css';
+
+function NaoEncontrada() {
+    return(
+        <section className={styles.container}>
+            <h1>Opss... Página Não Encontrada!</h1>
+        </section>
+            
+    )
+}
+
+export default NaoEncontrada;
